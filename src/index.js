fc0f98cf14663ada76d99b88d6c30751351c124d
SwegCet <ethantruong0804@gmail.com>
2024-07-08 03:20:46 -0700

    Added functions for home and menu page, about page is WIP

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
+import { loadHomePage } from "./pages/home";
+import { loadMenuPage } from "./pages/menu";
+import './styles/style.css';
+
+//Remove current content (if any)
+function removeContent() {
+    const content = document.querySelector('#content');
+    content.textContent = '';
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    loadHomePage();
+});
+
+
+const homeButton = document.querySelector('#home');
+
+const menuButton = document.querySelector('#menu');
+
+const aboutButton = document.querySelector('#about');
+
+
+//Add event listener on click of the buttons
+
+//Just do Home button for now
+homeButton.addEventListener('click', () => {
+    removeContent();
+    loadHomePage();
+});
+
+//menu button 
+menuButton.addEventListener('click', () => {
+    removeContent();
+    loadMenuPage();
+});
+
+//about button
+
 console.log('Prophet Save Me');
